c0b236d325518f2dd53b190f21554d8cfed73fa6
juliaokataleko <juliofeli78@gmail.com>
2021-06-03 23:57:24 +0000

    Add permissions and activities log packages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Administrador Geral',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345'),
+            'email_verified_at' => Carbon::now()
+        ]);
     }
 }
